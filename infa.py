79dbd8e8b1f65458a40161e983b1e75ded5e163a
Nezamov Stanislav <shaxraman@gmail.com>
2019-01-24 17:31:52 +0300

    Поиск в большем кол-ве директорий

--- infa.py
+++ infa.py
@@ -1,6 +1,24 @@
+import getpass
+
 serverName = 'icdc0.icore.local'
 domainName = 'icore'
-#userName = ''
+
+
+# userName = ''
 # password = ''
+
+def logging():
+    userName = input('Напишите Ваш логин ')
+    password = getpass.getpass()
+    #userName = 'snezamov'
+    #password = ''
+    if '@' in userName:
+        return userName[:userName.index('@')], password
+    elif '.i' in userName:
+        return userName[:userName.index('.i')], password
+    else:
+        return userName, password
+
+
 base = 'ou=domain users,dc=icore,dc=local'
 base2 = 'ou=domain computers,dc=icore,dc=local'
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import getpass
-
 from ldap3 import Server, Connection
 
 from infa import *
@@ -8,16 +6,11 @@ from infa import *
 def connect():
     while True:
         try:
-            userName = input('Напишите Ваш логин ')
-            if '@' in userName:
-                userName = userName[:userName.index('@')]
-            elif '.i' in userName:
-                userName = userName[:userName.index('.i')]
-            password = getpass.getpass()
+            log = logging()  # Вынесли логин и пароль в infa.py
             server = Server(serverName)
-            conn = Connection(server, read_only=True, user=f'{userName}@{domainName}', password=password,
+            conn = Connection(server, read_only=True, user=f'{log[0]}@{domainName}', password=log[1],
                               auto_bind=True)
-            print(f'Подключение к AD {serverName} под пользователем {userName} прошло успешно.\n')
+            print(f'Подключение к AD {serverName} под пользователем {log[0]} прошло успешно.\n')
             return conn
         except:
             print('Вы подпустили ошибку☻\n')
@@ -36,37 +29,48 @@ def search_user(user):
     print(userDN)
 
 
+ou = ['computers', 'domain computers', 'Domain Controllers', 'Domain Servers']
 # 212  LockedAccounts   154 without LockedAccounts
 res = {}
-
+oper_sys = []
 
 def search_os():
-    conn = connect()
-    oper_sys = []
-    search_filter = f'(&(objectClass=Computer))'
-    conn.search(base2, search_filter, attributes=['operatingSystem'])
-    a = 'Всего операционных систем в ou=domain computers', len(conn.response)
-    for i in conn.response:
-        if 'LockedAccounts' in i['dn']:
-            continue
-        oper_sys.append(i['attributes']['operatingSystem'])
-        print(i)
-    conn.unbind()
-    print('\n\n', a)
-    print('Не считая LockedAccounts ', len(oper_sys))
+
+    for kk in ou:
+        conn = connect()
+        search_filter = f'(&(objectClass=Computer))'
+        base1 = f'ou={kk},dc=icore,dc=local'
+        conn.search(base1, search_filter, attributes=['operatingSystem'])
+        a = 'Всего операционных систем в ou=domain computers', len(conn.response)
+        for i in conn.response:
+            if 'LockedAccounts' in i['dn']:
+                continue
+            oper_sys.append(i['attributes']['operatingSystem'])
+            print(i)
+        print('\n\n', a)
+        print('Не считая LockedAccounts ', len(oper_sys))
+        conn.unbind()
     for i in oper_sys:
-        if i not in res.keys():
-            res[i] = 1  # Добавляем ключ: значение в словарь
-        else:
-            res[i] = res.get(i) + 1  # Обновляем значение у выбранного ключа
+        #print(res.keys())
+        try:
+            if i not in res.keys():
+                res[i] = 1  # Добавляем ключ: значение в словарь
+            else:
+                res[i] = res.get(i) + 1  # Обновляем значение у выбранного ключа
+        except TypeError:
+            print('какая-то шибка')
+
+    print('os = ')
     [print(i) for i in res.items()]
+
+
     return res
 
 
 def main():
     search_os()
-
-    input('Нажмите любую клавишу для выхода.')
+    #[print(i) for i in res.items()]
+    # input('Нажмите любую клавишу для выхода.')
 
 
 if __name__ == '__main__':
