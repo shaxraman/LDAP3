2e881bf9994a6ed63a9fe8a1cc1f265f8f458eb0
Stanislav Nezamov <shaxraman@gmail.com>
2020-03-13 12:09:00 +0300

    New_function

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 from pprint import pprint
-from ldap3 import Server, Connection, MODIFY_REPLACE
+from ldap3 import Server, Connection, MODIFY_REPLACE    
 
 from infa import *
 
 
 def connect():
     """ Подключение к AD """
-    while True:
-        try:
-            log = logging()  # Вынесли логин и пароль в infa.py
-            server = Server(serverName)
-            conn = Connection(server, read_only=False, user=f'{log[0]}@{domainName}', password=log[1],
-                              auto_bind=True)
-            print(
-                f'Подключение к AD {serverName} под пользователем {log[0]} прошло успешно.\n')
-            return conn
-        except:
-            print('Вы подпустили ошибку☻\n')
-            continue
-        break
+    try:
+        log = logging()  # Вынесли логин и пароль в infa.py
+        server = Server(serverName)
+        conn = Connection(server, read_only=False, user=f'{log[0]}@{domainName}', password=log[1],
+                            auto_bind=True)
+        print(
+            f'Подключение к AD {serverName} под пользователем {log[0]} прошло успешно.\n')
+        return conn
+    except:
+        print('Вы подпустили ошибку☻\n')
 
 
 def search_ext1(user):
@@ -36,45 +33,58 @@ def search_ext1(user):
 
 
 def search_ext1_all():
-    """ Ищет всех пользователей и выводит их атрибуты"""
+    """ Ищет всех пользователей и выводит их атрибуты"""  # (&(objectClass=user)(msDS-parentdistname=OU=Domain Users,DC=icore,DC=local)) - Все активные
     conn = connect()
     all_user = None
-    search_filter = f'(&(objectClass=user)(msDS-parentdistname=OU=Domain Users,DC=icore,DC=local))'  # f'(&(objectClass=user)(msDS-parentdistname=OU=Domain Users,DC=icore,DC=local))'  f'(&(objectClass=user))'
+    search_filter = f'(&(objectClass=user))'
     x = conn.search(base, search_filter, attributes=[
-                    'extensionAttribute1', 'distinguishedName', 'telephoneNumber', 'ipPhone'])
+                    'extensionAttribute1', 'distinguishedName', 'telephoneNumber'])
     if x != False:
         all_user = (conn.response)
     conn.unbind()
     return all_user
 
 
+def search_proxyAddresses():
+    """ Ищет всех пользователей и выводит их атрибуты"""  # (&(objectClass=user)(msDS-parentdistname=OU=Domain Users,DC=icore,DC=local)) - Все активные
+    conn = connect()
+    all_user = None
+    search_filter = f'(&(objectClass=user)(msDS-parentdistname=OU=Domain Users,DC=icore,DC=local))'
+    x = conn.search(base, search_filter, attributes=[
+                    'proxyAddresses', 'distinguishedName', 'sAMAccountName'])
+    if x != False:
+        all_user = (conn.response)
+    conn.unbind()
+    return all_user
+
+
+def add_atr(all_users):
+    """ Принимает список юзеров с их атрибутами proxyAddresses и заменяет определенные """
+    conn = connect()
+    for user in all_users:
+        new_list = []
+        for i in user['attributes']['proxyAddresses']:
+            new_list.append(i)
+
+        acc = user['attributes']['sAMAccountName']
+        new = f'smtp:{str(acc)}@o365.i-core.ru'
+        new_list.append(new)
+        print('----\n', new_list)
+        # (conn.modify(юзер у кого меняем), {что меняем: (MODIFY_REPLACE, На что меняем (список))}
+        print(conn.modify(user['attributes']['distinguishedName'], {'proxyAddresses': (
+            MODIFY_REPLACE, new_list)}))
+        print('\n---\n')
+    conn.unbind()
+
+
 def replace_attribute(all_users):
     """ Принимает список юзеров с их атрибутами (search_ext1_all) и заменяет определенные """
     conn = connect()
     for user in all_users:
-        try:
-            if user['attributes']['telephoneNumber']:
-                print(user['attributes']['extensionAttribute1'])
-                print(conn.modify(user['attributes']['distinguishedName'], {'extensionAttribute1': (
-                    MODIFY_REPLACE, [str(3) + str(user['attributes']['telephoneNumber'])])}))
-        except Exception as e:
-            pprint(e)
-
-    conn.unbind()
-
-
-def replace_ip_phone_attribute(all_users):
-    """ Принимает список юзеров с их атрибутами (ip_phone) и заменяет определенные """
-    conn = connect()
-    for user in all_users:
-        try:
-            if user['attributes']['telephoneNumber']:
-                print(user['attributes']['ipPhone'])
-                print(conn.modify(user['attributes']['distinguishedName'], {'ipPhone': (
-                    MODIFY_REPLACE, [str(1) + str(user['attributes']['telephoneNumber'])])}))
-        except Exception as e:
-            pprint(e)
-
+        if user['attributes']['extensionAttribute1']:
+            print(user['attributes']['extensionAttribute1'])
+            print(conn.modify(user['attributes']['distinguishedName'], {'extensionAttribute1': (
+                MODIFY_REPLACE, [str(3) + user['attributes']['telephoneNumber']])}))
     conn.unbind()
 
 
@@ -152,8 +162,20 @@ def search_os():
     return res
 
 
+def search_2(user):
+    """ Меняем что-то у конкретного юзера"""
+    conn = connect()
+    userDN = None
+    search_filter = f'(&(objectClass=user)(sAMAccountName={user}))'
+    x = conn.search(base, search_filter, attributes=[
+                    'extensionAttribute1', 'distinguishedName', 'proxyAddresses'])
+    if x != False:
+        userDN = (conn.response[0]['attributes']['extensionAttribute1'],
+                  conn.response[0]['attributes']['distinguishedName'],
+                  conn.response[0]['attributes']['proxyAddresses'])
+    print(conn.modify('CN=Stanislav Nezamov,OU=Domain Users,DC=icore,DC=local', {'proxyAddresses': (MODIFY_REPLACE, ['3769', 'smtp:o365.i-core.ru'])}))   # замена атрибута
+    print(userDN)
+
 if __name__ == '__main__':
-    all = search_ext1_all()
-    # pprint(all)
-    replace_ip_phone_attribute(all)
-    replace_attribute(all)
+    # all = search_proxyAddresses()
+    # pprint(add_atr(all))
